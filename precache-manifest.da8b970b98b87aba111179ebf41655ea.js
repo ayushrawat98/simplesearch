a1299261b2dc25176c6fb84d707115e44f2ccc13
ayushrawat98 <aayushrawat0@gmail.com>
2020-04-28 11:54:02 +0530

    Updates

---
+++ precache-manifest.da8b970b98b87aba111179ebf41655ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634467fdb175e159d60d065e7585b627",
+    "url": "/simplesearch/index.html"
+  },
+  {
+    "revision": "da8c9b8fd8fcdd13c56b",
+    "url": "/simplesearch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdd2148972a4fc00571a",
+    "url": "/simplesearch/static/js/2.d8e8e361.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplesearch/static/js/2.d8e8e361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8c9b8fd8fcdd13c56b",
+    "url": "/simplesearch/static/js/main.ddd84ebf.chunk.js"
+  },
+  {
+    "revision": "7d0998fd8d38775b7dd9",
+    "url": "/simplesearch/static/js/runtime-main.a108cd8d.js"
+  }
+]);
